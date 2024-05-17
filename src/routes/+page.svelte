83a132daef92b281cbc2a669e4af81b570f71712
patrickwaldera <patrickwaldera@gmail.com>
2024-05-17 20:15:41 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		Hello World
+	</h1>
+</section>
