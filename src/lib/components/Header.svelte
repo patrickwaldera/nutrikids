b7be4f4b3da699594ae82bc4befe56a1bdf64217
patrickwaldera <patrickwaldera@gmail.com>
2024-05-18 17:56:05 -0300

    refactor: change navbar to component

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -8,34 +8,3 @@
 		<span>Nutri<b class="text-emerald-600">Kids</b></span>
     </a>
 </nav>
-
-<nav class="flex justify-center bg-base-200">
-    <ul class="menu menu-horizontal justify-center">
-        <li>
-            <a href="/">
-                <i class='bx bx-user'></i>
-                Shop
-            </a>
-        </li>
-        <li>
-            <a href="/">
-                Catalog 
-            </a>
-        </li>
-        <li>
-            <a href="/">
-                News 
-            </a>
-        </li>
-        <li>
-            <a href="/">
-                Blog 
-            </a>
-        </li>
-        <li>
-            <a href="/">
-                Team 
-            </a>
-        </li>
-    </ul>
-</nav>
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,40 @@
+<nav class="flex justify-center bg-base-200">
+    <ul class="menu menu-horizontal justify-center">
+        <li>
+            <a href="/">
+                <i class='bx bxs-pie-chart-alt-2'></i>
+                Visão Geral
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                <i class='bx bx-clipboard' ></i>
+                Registros
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                <i class='bx bx-merge'></i>
+                Turmas 
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                <i class='bx bxs-graduation'></i>
+                Alunos 
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                <i class='bx bxs-user-circle'></i>
+                Perfil 
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                <i class='bx bx-log-out'></i>
+                Sair
+            </a>
+        </li>
+    </ul>
+</nav>
