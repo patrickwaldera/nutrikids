1e28fda487b1f4568be4bc3e1e0477cef0f90e5d
patrickwaldera <patrickwaldera@gmail.com>
2024-05-18 20:07:02 -0300

    refactor: create object before

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -4,13 +4,14 @@ import { UserStore } from '$lib/stores/UserStore'
 pb.authStore.loadFromCookie(document.cookie)
 pb.authStore.onChange(() => {
 	if (pb.authStore.isValid) {
-		UserStore.set({
+		const user = {
 			id: pb.authStore.model?.id,
 			name: pb.authStore.model?.name,
 			username: pb.authStore.model?.username,
 			email: pb.authStore.model?.email,
 			role: pb.authStore.model?.role
-		})
+		}
+		UserStore.set(user)
 	} else {
 		UserStore.set(undefined)
 	}
