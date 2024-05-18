9003a23c2223fd047a4cce10371c0d84977edd77
patrickwaldera <patrickwaldera@gmail.com>
2024-05-17 23:50:08 -0300

    feat: add header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,41 @@
+<script>
+	import logo from '$lib/images/nutrikids.png';
+</script>
+
+<nav class="navbar justify-center bg-base-300">
+    <a class="btn btn-ghost text-lg" href="/">
+        <img alt="Logo" src={logo} class="w-12" />
+		<span>Nutri<b class="text-emerald-600">Kids</b></span>
+    </a>
+</nav>
+
+<nav class="flex justify-center bg-base-200">
+    <ul class="menu menu-horizontal justify-center">
+        <li>
+            <a href="/">
+                <i class='bx bx-user'></i>
+                Shop
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                Catalog 
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                News 
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                Blog 
+            </a>
+        </li>
+        <li>
+            <a href="/">
+                Team 
+            </a>
+        </li>
+    </ul>
+</nav>
