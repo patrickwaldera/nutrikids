f9ab540082b8dd2c78a64d300f53c177177fd5b0
patrickwaldera <patrickwaldera@gmail.com>
2024-05-18 18:00:52 -0300

    feat: add types in Locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,16 @@
 // See https://kit.svelte.dev/docs/types#app
+
+import type { User } from "$lib/core/entities/User";
+
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		type PocketBase = import("pocketbase").default;
+		interface Locals {
+			pb?: PocketBase;
+			user?: User;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
