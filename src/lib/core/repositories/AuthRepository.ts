27840164ecdfb6696edb7980a41bc4a171eb7de2
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 15:32:42 -0300

    feat: update auth repository

--- src/lib/core/repositories/AuthRepository.ts
+++ src/lib/core/repositories/AuthRepository.ts
@@ -1,3 +1,5 @@
+import type { LoginResponse } from "../dtos/LoginResponse";
+
 export interface IAuthRepository {
-	login(username: string, password: string): Promise<any>;
+	login(username: string, password: string): Promise<LoginResponse>;
 }
--- src/lib/infra/repositories_impl/PBAuthRepository.ts
+++ src/lib/infra/repositories_impl/PBAuthRepository.ts
@@ -1,15 +1,34 @@
-import type { IAuthRepository } from "$lib/core/repositories/AuthRepository";
 import axios from "axios";
 import { API_BASE_URL } from "./pocketbase";
+import type { IAuthRepository } from "$lib/core/repositories/AuthRepository";
+import type { LoginResponse } from "$lib/core/dtos/LoginResponse";
 
 export class PBAuthRepository implements IAuthRepository {
-	public async login(username: string, password: string) {
+	public async login(username: string, password: string): Promise<LoginResponse> {
 		try {
 			const response = await axios.post(`${API_BASE_URL}/collections/users/auth-with-password`, {
 				identity: username,
 				password: password,
-			});
-			return response.data;
+			});		
+			const data = response.data;
+
+			const user = {
+				id: data.record.id,
+				name: data.record.name,
+				username: data.record.username,
+				email: data.record.email,
+				role: data.record.role,
+				school_id: data.record.school_id,
+			};
+
+			const token = response.data.token;
+
+			if (!token) {
+				throw new Error("Invalid token");
+			}
+
+			return { user, token };
+
 		} catch (error) {
 			throw error;
 		}
