aba8348d7fa663fe0469995e1f58942a80ee0428
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 00:20:35 -0300

    refactor: use api instead of sdk

--- src/lib/infra/repositories_impl/PBAuthRepository.ts
+++ src/lib/infra/repositories_impl/PBAuthRepository.ts
@@ -1,13 +1,17 @@
 import type { IAuthRepository } from "$lib/core/repositories/AuthRepository";
-import { pb } from "./pocketbase";
+import axios from "axios";
+import { API_BASE_URL } from "./pocketbase";
 
 export class PBAuthRepository implements IAuthRepository {
-
 	public async login(username: string, password: string) {
-		await pb.collection("users").authWithPassword(username, password);
-	}
-
-	public logout(): any {
-		pb.authStore.clear();
+		try {
+			const response = await axios.post(`${API_BASE_URL}/collections/users/auth-with-password`, {
+				identity: username,
+				password: password,
+			});
+			return response.data;
+		} catch (error) {
+			throw error;
+		}
 	}
 }
