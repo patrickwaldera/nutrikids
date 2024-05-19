992d0b33234d958d1d7e2d5d1af9b2846ab7bf4e
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 00:19:19 -0300

    feat: add method to handle token expiration

--- src/lib/core/services/AuthService.ts
+++ src/lib/core/services/AuthService.ts
@@ -4,11 +4,22 @@ import type { IAuthRepository } from "../repositories/AuthRepository";
 export class AuthService {
 	private static authRepository: IAuthRepository = new PBAuthRepository();
 
-	public static async login(username: string, password: string) {
-		await AuthService.authRepository.login(username, password);
+	public static async login(username: string, password: string): Promise<any> {
+		try {
+			return await AuthService.authRepository.login(username, password);
+		} catch (error) {
+			throw error;
+		}
 	}
 
-	public static logout() {
-		AuthService.authRepository.logout();
+	public static isTokenExpired(token: string): boolean {
+		const decodedToken = AuthService.decodeToken(token);
+		const expiration = decodedToken.exp;
+		const currentTime = Math.floor(Date.now() / 1000);
+		return expiration < currentTime;
+	}
+
+	public static decodeToken(token: string): any {
+		return JSON.parse(atob(token.split(".")[1]));
 	}
 }
