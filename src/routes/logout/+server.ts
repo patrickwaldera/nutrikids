189f962b8d966dcaa07919adc9543d57a1cd33a4
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 00:21:37 -0300

    refactor: logout

--- src/routes/logout/+server.ts
+++ src/routes/logout/+server.ts
@@ -1,8 +1,8 @@
 import { redirect } from "@sveltejs/kit";
 
-export const POST = async ({ locals }: any) => {
-	locals.user = undefined;
-	locals.pb.authStore.clear();
-	
+export const POST = async ({ locals, cookies }: any) => {
+	locals.token = null;
+	cookies.delete("token", { path: "/" });
+	cookies.delete("user", { path: "/" });
 	throw redirect(303, "/login")
 }
