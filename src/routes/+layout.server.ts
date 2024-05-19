dd0b7825568cbb2a28e7718eda1a86522e004492
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 00:22:12 -0300

    feat: handle token and user from cookie

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,22 @@
+
+import { AuthService } from "$lib/core/services/AuthService";
+import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ locals }) => {
-	return {
-	  user: locals.user,
+export const load: LayoutServerLoad = async ({cookies}) => {
+
+	const tokenCookie = cookies.get("token") || null;
+	if (tokenCookie !== null) {
+		const isTokenExpired = AuthService.isTokenExpired(tokenCookie);
+		if(isTokenExpired) {
+			cookies.delete("token", { path: "/" });
+			cookies.delete("user", { path: "/" });
+		}
 	}
-  }
+	
+	const userCookie = cookies.get("user") || null;
+	const user = userCookie ? JSON.parse(userCookie) : null;
+	return {
+		user
+	}
+}
