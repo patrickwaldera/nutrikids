07eb9f630dab7d85fee37a501b6e7769d55a8c2d
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 00:19:44 -0300

    refactor: remove sdk pocketbase

--- src/lib/infra/repositories_impl/pocketbase.ts
+++ src/lib/infra/repositories_impl/pocketbase.ts
@@ -1,8 +1,1 @@
-import { PUBLIC_POCKETBASE_URL } from "$env/static/public";
-import PocketBase from "pocketbase";
-
-export function createInstance() {
-	return new PocketBase(PUBLIC_POCKETBASE_URL)
-  }
-  
-  export const pb = createInstance()
+export const API_BASE_URL = "https://pocketbase.walphi.com/api";
