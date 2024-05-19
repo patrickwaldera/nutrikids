c451e1d0fdb7251ef916b39c67a98c7b71c71027
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 15:33:27 -0300

    feat: get school info

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,19 @@
 import { redirect } from "@sveltejs/kit"
 import type { PageServerLoad } from "./$types"
+import { SchoolService } from "$lib/core/services/SchoolService";
 
 export const prerender = true;
 
-export const load: PageServerLoad = async ({ locals }: any) => {
-	if(!locals.token) {
+export const load: PageServerLoad = async (event) => {
+	if(!event.locals.token) {
 	  throw redirect(303, "/login")
 	}
+
+	const schoolId = event.locals.user?.school_id;
+	if (schoolId) {
+		const school = await SchoolService.getOneById(event.locals.token, schoolId);
+		return { school };
+	}
+
+	return { error: "School not found" };
   }
