9a367e06dbcfcd38c7508420e30e62a627b7376b
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 21:27:26 -0300

    feat: add skeleton component

---
+++ src/lib/components/SkeletonTable.svelte
@@ -0,0 +1,12 @@
+<div class="flex flex-col gap-4 w-full">
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+	<div class="skeleton h-4 w-full"></div>
+</div>
