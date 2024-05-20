352a39b51a63bcd8bf52ff9f09482c96e81f5d0a
patrickwaldera <patrickwaldera@gmail.com>
2024-05-19 21:33:18 -0300

    feat: add secure

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,16 +1,18 @@
 
 import { AuthService } from "$lib/core/services/AuthService";
-import { redirect } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
+import { ENVIRONMENT } from "$env/static/private";
 
 export const load: LayoutServerLoad = async ({cookies}) => {
 
 	const tokenCookie = cookies.get("token") || null;
 	if (tokenCookie !== null) {
 		const isTokenExpired = AuthService.isTokenExpired(tokenCookie);
+		
+		const secure = ENVIRONMENT === "production" ? true : false;
 		if(isTokenExpired) {
-			cookies.delete("token", { path: "/" });
-			cookies.delete("user", { path: "/" });
+			cookies.delete("token", { path: "/", secure: secure });
+			cookies.delete("user", { path: "/", secure: secure });
 		}
 	}
 	
