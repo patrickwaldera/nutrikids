1246bf357b539daadad26486b88463dfb952d1f1
patrickwaldera <patrickwaldera@gmail.com>
2024-05-21 20:45:20 -0300

    fix: bmi status

--- src/lib/core/services/BmiService.ts
+++ src/lib/core/services/BmiService.ts
@@ -11,7 +11,7 @@ export class BmiService {
         if (bmi <= 16.9) return "Muito abaixo do peso";
 		if (bmi >= 17 && bmi <= 18.4) return "Abaixo do peso";
 		if (bmi >= 18.5 && bmi <= 24.9) return "Peso normal";
-		if (bmi >= 25 && bmi <= 29.9) return "Sobrepeso";
+		if (bmi >= 25 && bmi <= 29.9) return "Acima do peso";
 		if (bmi >= 30 && bmi <= 34.9) return "Obesidade grau I";
 		if (bmi >= 35 && bmi <= 39.9) return "Obesidade grau II";
 		if (bmi >= 40) return "Obesidade grau III";
