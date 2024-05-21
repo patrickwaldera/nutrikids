e2da32c7d6c4cb1e7e5deef69359389d9a1a98eb
patrickwaldera <patrickwaldera@gmail.com>
2024-05-20 22:08:02 -0300

    feat: add functions to handle date

--- src/lib/core/utils/Date.ts
+++ src/lib/core/utils/Date.ts
@@ -6,3 +6,26 @@ export function getMonthName(currentYear: number, month: number) {
 export function formatDateToYYYYMMDD(date: Date) {
     return date.toISOString().split('T')[0];
 }
+
+export function validateDateFormat(date: string) {
+	const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+	return regex.test(date);
+}
+
+export function convertDateToYYYYMMDD(date: string) {
+	try {
+		const parts = date.split("/");
+		return `${parts[2]}-${parts[1]}-${parts[0]}`;
+	} catch (error) {
+		throw error;
+	}
+}
+
+export function convertDateToDDMMYYYY(date: string) {
+	try {
+		const parts = date.split("-");
+		return `${parts[2]}/${parts[1]}/${parts[0]}`;
+	} catch (error) {
+		throw error;
+	}
+}
