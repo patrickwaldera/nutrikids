4796368a86c23713526a66334eca89b506caebd6
patrickwaldera <patrickwaldera@gmail.com>
2024-05-22 21:27:16 -0300

    feat: add select class

--- src/lib/components/EditStudentModal.svelte
+++ src/lib/components/EditStudentModal.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
-    import { validateDateFormat } from '$lib/core/utils/Date';
+    import type { Class } from '$lib/core/entities/Class';
+    import type { Student } from '$lib/core/entities/Student';
+    import { formatDateInput, validateDateFormat } from '$lib/core/utils/Date';
 	import { createEventDispatcher } from 'svelte';
     import { fade } from 'svelte/transition';
 
-	export let student;
+	export let student: Student | null = null;
+	export let classes: Class[] = [];
 
     const dispatch = createEventDispatcher();
 
@@ -16,11 +19,19 @@
     }
 
 	async function saveChanges() {
-		const isDateValid = validateDateFormat(updatedStudent.birthDate);
-		if (!isDateValid) {
-			errorDateMessage = `O formato da data é inválido! Use o formato ${new Date().toLocaleDateString("pt-BR")}.`;
-			return;
+		if (updatedStudent.birthDate !== "") {
+			const isDateValid = validateDateFormat(updatedStudent?.birthDate!);
+			if (!isDateValid) {
+				errorDateMessage = `O formato da data é inválido! Use o formato ${new Date().toLocaleDateString("pt-BR")}.`;
+				return;
+			}
 		}
+		classes?.forEach((classItem) => {
+			if (updatedStudent.classId === classItem.id) {
+				updatedStudent.className = classItem.name;
+				updatedStudent.classAlias = classItem.alias;
+			}
+		})
         dispatch('save', updatedStudent);
         closeModal();
     }
@@ -51,10 +62,22 @@
 					type="text"
 					class="input-sm input-bordered w-full border-2 rounded-md"
 					bind:value={updatedStudent.birthDate}
+					on:input={formatDateInput}
 				/>
 				{#if errorDateMessage}
 					<p transition:fade={{ duration: 300 }} class="text-xs text-error mt-2">{errorDateMessage}</p>
 				{/if}
+			</div>
+			<div class="form-control w-full">
+				<label class="label" for="date">
+					<span class="label-text">Turma</span>
+				</label>
+				<select class="select select-bordered select-sm" bind:value={updatedStudent.classId}>
+					<option value="default" disabled selected>Selecione uma turma</option>
+					{#each classes ?? [] as {id, name}}
+						<option value={id}>{name}</option>
+					{/each}
+				</select>
 			</div>
 			<div class="form-control w-full flex flex-col gap-2 mt-6">
 				<button type="submit" class="btn btn-primary w-full">Salvar</button>
