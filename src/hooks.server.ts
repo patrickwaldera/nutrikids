cc4361e67f445e0c1aa56f820a3656e3f08129f6
patrickwaldera <patrickwaldera@gmail.com>
2024-05-23 19:10:19 -0300

    refactor: organize

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,7 @@
 import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
+	
 	event.locals.token =  event.cookies.get("token") || null;
 
 	const userCookie = event.cookies.get("user") || null;
