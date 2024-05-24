edfc8bffbfb6df81718a9062cb570b948ccd1d99
patrickwaldera <patrickwaldera@gmail.com>
2024-05-23 22:20:48 -0300

    fix: center login vertically

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -15,36 +15,38 @@
 	<title>NutriKids - Login</title>
 </svelte:head>
 
-<form method="POST" action="?/login" transition:fade={{ duration: 200 }} class="flex flex-col gap-4 rounded-box bg-base-200 p-6 max-w-md w-full">
-    <h1 class="text-3xl font-bold self-center">Log in</h1>
-
-    <label class="form-control">
-        <div class="label">
-            <span class="label-text">Nome de usuário ou email</span>
-        </div>
-
-        <input name="username" class="input input-bordered" bind:value={username} />
-    </label>
-
-    <label class="form-control">
-        <div class="label">
-            <span class="label-text">Senha</span>
-        </div>
-
-        <input name="password" type="password" class="input input-bordered" bind:value={password} />
-    </label>
-
-    {#if form?.error}
-        <div transition:fade={{ duration: 300 }} class="badge badge-error gap-2 h-auto max-h-14 w-full">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-            {form?.error}
-        </div>
-    {/if}
-
-    {#if $isLoading}
-        <span class="loading loading-dots loading-lg self-center"></span>
-    {:else}
-        <button class="btn btn-primary" {disabled}>Log in</button>
-    {/if}
-
-</form>
+<section in:fade={{ duration: 300 }} class="flex items-center justify-center m-auto w-full">
+    <form method="POST" action="?/login" transition:fade={{ duration: 200 }} class="flex flex-col gap-4 rounded-box bg-base-200 p-6 max-w-md w-full">
+        <h1 class="text-3xl font-bold self-center">Log in</h1>
+    
+        <label class="form-control">
+            <div class="label">
+                <span class="label-text">Nome de usuário ou email</span>
+            </div>
+    
+            <input name="username" class="input input-bordered" bind:value={username} />
+        </label>
+    
+        <label class="form-control">
+            <div class="label">
+                <span class="label-text">Senha</span>
+            </div>
+    
+            <input name="password" type="password" class="input input-bordered" bind:value={password} />
+        </label>
+    
+        {#if form?.error}
+            <div transition:fade={{ duration: 300 }} class="badge badge-error gap-2 h-auto max-h-14 w-full">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-4 h-4 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path></svg>
+                {form?.error}
+            </div>
+        {/if}
+    
+        {#if $isLoading}
+            <span class="loading loading-dots loading-lg self-center"></span>
+        {:else}
+            <button class="btn btn-primary" {disabled}>Log in</button>
+        {/if}
+    
+    </form>
+</section>
