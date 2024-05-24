d9dd2e1b7749b136abf2eae9ba8bcd26c8954932
patrickwaldera <patrickwaldera@gmail.com>
2024-05-23 23:50:36 -0300

    fix: config cors

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,6 +9,9 @@ export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.user =  user;
 
 	const response = await resolve(event);
+	response.headers.set('Access-Control-Allow-Origin', '*')
+	response.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+	response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 	response.headers.set('cache-control', 'no-cache')
 	
 	return response;
